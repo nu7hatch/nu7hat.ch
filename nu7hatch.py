f8dadfbefac68f13875f8a8b1115b04692f8da65
Kriss Kowalik <kriss.kowalik@gmail.com>
2010-11-07 05:08:52 +0100

    Added analytics tracking

--- nu7hatch.py
+++ nu7hatch.py
@@ -8,12 +8,17 @@ sys.path = ['lib/itty'] + sys.path
 from itty import get, run_itty
 from google.appengine.ext.webapp import template
 
+
+analytics = open('misc/analytics').read()
+
 def render(name, params={}):
     return template.render('templates/%s.html' % name, params)
 
+
 @get('/')
 def index(request):
-    return render('index', {'face': open('misc/face.ascii').read()})
+    face = open('misc/face').read()
+    return render('index', {'face': face, 'analytics': analytics})
     
 @get('/blog')
 def blog(request):
@@ -21,7 +26,8 @@ def blog(request):
     
 @get('/blog/:slug')
 def blog_entry(request):
-    pass    
+    pass
+    
     
 def main():
     run_itty('appengine')
